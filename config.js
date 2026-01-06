8c18215c5121b6ee03b7d8bb7646f7d8f535a4ed
mtakuma1 <tontankuma@gmail.com>
2026-01-06 21:12:18 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window.CONFIG = {
+    moveSpeed: 5,      // 左右の速さ
+    jumpPower: 14,     // ジャンプの高さ
+    gravity: 0.7,      // 重力の強さ
+    
+    // 敵の挙動
+    enemyShotInterval: 1500, // 敵が弾を撃つ間隔（ミリ秒）
+    bulletSpeed: 7           // 弾の速さ
+};
+
